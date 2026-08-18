1c886fa06efbbb59bb5bf04f3759391d064b347b
Harolde <haroldlascuna@gmail.com>
2026-08-18 15:04:16 +0800

    fix: use dynamic import and cached handler in api/index.js to resolve ERR_REQUIRE_ESM

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-import app from '../backend/dist/index.js';
+let cachedApp;
 
-export default app;
+export default async function handler(req, res) {
+  if (!cachedApp) {
+    const mod = await import('../backend/dist/index.js');
+    cachedApp = mod.default || mod;
+  }
+  return cachedApp(req, res);
+}
